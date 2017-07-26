0583e7bc1293bdc720cc0833c69b98af23f78819
Jimmy Lee <jimmy-lee@live.com>
2017-07-26 13:41:04 -0700

    /api/authenticated to check if authed

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import post from './controllers/post';
 import comment from './controllers/comment';
 import passport from 'passport';
 
-const authenticated = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   } else {
@@ -20,34 +20,46 @@ module.exports = app => {
     });
   });
 
+  app.get('/api/authenticated', (req, res) => {
+    if (req.isAuthenticated()) {
+      return res.status(200).send({
+        isAuthenticated: true,
+      });
+    }
+
+    return res.status(403).send({
+      isAuthenticated: false,
+    });
+  });
+
   app.post('/api/signup', user.create);
   app.post('/api/login', user.auth);
-  app.post('/api/logout', authenticated, user.logout);
+  app.post('/api/logout', authMiddleware, user.logout);
 
   app.post('/api/users', user.create);
   app.get('/api/users', user.list);
   app.get('/api/users/:userId', user.get);
-  app.put('/api/users/:userId', authenticated, user.update);
-  app.delete('/api/viewer/delete', authenticated, user.deleteViewer);
+  app.put('/api/users/:userId', authMiddleware, user.update);
+  app.delete('/api/viewer/delete', authMiddleware, user.deleteViewer);
 
-  app.post('/api/posts', authenticated, post.create);
+  app.post('/api/posts', authMiddleware, post.create);
   app.get('/api/posts', post.list);
   app.get('/api/posts/:postId', post.get);
-  app.put('/api/posts/:postId', authenticated, post.update);
-  app.delete('/api/posts/:postId', authenticated, post.delete);
+  app.put('/api/posts/:postId', authMiddleware, post.update);
+  app.delete('/api/posts/:postId', authMiddleware, post.delete);
 
   app.get('/api/comments', comment.list);
-  app.post('/api/comments', authenticated, comment.create);
+  app.post('/api/comments', authMiddleware, comment.create);
   app.get('/api/posts/:postId/comments', comment.getAll);
   app.get('/api/posts/:postId/comments/:commentId', comment.get);
   app.put(
     '/api/posts/:postId/comments/:commentId',
-    authenticated,
+    authMiddleware,
     comment.update
   );
   app.delete(
     '/api/posts/:postId/comments/:commentId',
-    authenticated,
+    authMiddleware,
     comment.delete
   );
 };
